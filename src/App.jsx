16f3753cace278256603457cdffa4d3a8601f09d
shivang <shivang.ramola.hp@gmail.com>
2024-03-07 20:56:59 +0530

    feat(src): added qrcode compoenent
    added qrcode compoenent with working of package qr code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import "./App.css";
+import QrCode from "./QrCode";
 
-function App() {
-  return (
-    <>
-      <h1 className="text-6xl">React App</h1>
-      <p className="text-4xl pt-10">Hello React Developers</p>
-    </>
-  );
-}
+const App = () => <QrCode />;
 
 export default App;
