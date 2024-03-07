8dc6d0b24f89b4ee078f49418312f158c3cad923
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-03-07 14:14:11 +0530

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,13 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+// eslint-disable-next-line no-undef
+module.exports = withMT({
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
 export const mode = "jit";
 export const purge = ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"];
 export const darkMode = false;
