f1d85c920302a72e5da52027469640bc063dae68
Gunjan Gangwar <gangwargunjan9@gmail.com>
2024-03-09 11:20:36 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import QrCode from "./QrCode";
 
 function App() {
   return (
     <>
-      <h1 className="text-6xl">React App</h1>
-      <p className="text-4xl pt-10">Hello React Developers</p>
+      <h1 className="text-6xl">React QR Code APP</h1>
+      <p className="text-3xl pt-10">Enter some Text To See QR Code</p>
+      <QrCode />
     </>
   );
 }
--- src/QrCode.jsx
+++ src/QrCode.jsx
@@ -8,14 +8,17 @@ const QrCode = () => {
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="mt-10">
         <input
           type="text"
           value={text}
           onChange={(e) => setText(e.target.value)}
+          className="mt-8 bg-white text-black"
         />
       </form>
-      {text !== "" && <QRCode value={text} />}
+      {text !== "" && (
+        <QRCode value={text} className="mt-5 ml-32 border-white" />
+      )}
     </div>
   );
 };
